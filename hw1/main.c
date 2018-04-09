d74012b8b15d129f02c97ebad95e6fcf07de9bc1
root <Hyun Goo Kang>
2018-04-10 01:00:40 +0900

    complete change mode

--- hw1/main.c
+++ hw1/main.c
@@ -13,15 +13,15 @@
 #define SEM_SIZE 4096
 
 #define BACK 158
-#define VOL+ 115
-#define VOL- 114
+#define VOL_PLUS 115
+#define VOL_MINUS 114
 
 int main(){
 // prepare variables
 	char buf[SEM_SIZE];
 	int mode=1;	// 초기모드 #1(clock) / fork()후 각 프로세스가 자신의 mode 변수를 가진다.
 	int mode_key;			// readkey()함수 리턴값 받는데 사용
-	int i;
+	//int i;
 // prepare semaphore
 	int sem_input,sem_main,sem_output;
 	sem_input = initsem(IPC_PRIVATE,1);		// init_val = 1
@@ -36,10 +36,12 @@ int main(){
 		perror("shmget");
 		exit(1);
 	}
+	/*
 	shmaddr = (char *)shmat(shmid, NULL, 0);	// 0 = read/write
 	for(i=0;i<SEM_SIZE;i++){
 		shmaddr[i] = '0';	// clear shared memory
 	}
+	*/
 // prepare fork
 	pid_t pid;
 
@@ -48,40 +50,49 @@ int main(){
 // INPUT PROCESS - child 1
 	if(pid==0){	
 		while(1){
-			//shmaddr = (char *)shmat(shmid, NULL, 0);	// 0 = read/write
+			shmaddr = (char *)shmat(shmid, NULL, 0);	// 0 = read/write
 			semlock(sem_input);
 				/*
 				strcpy(shmaddr, "child1 input");
 				printf("INPUT PROCESS : %s\n\n", shmaddr);
 				*/
+				//printf("-------enter readkey()----------\n");
 				mode_key = readkey();
+				printf("mode_key = %d\n", mode_key);
 				switch(mode_key){
 					case BACK :	// exit program
 						shmaddr[0] = mode;
 						shmaddr[1] = mode_key;
 						shmaddr[2] = '\0';
+						printf("input - mode = %d\t mode_key = %d\n",shmaddr[0], shmaddr[1]);
 						shmdt(shmaddr);	// detach shared memory
 						// device is closed in readkey() above.
-						exit(0);
+						//exit(0);
+						//return 0;
 						break;
-					case VOL+ : // add mode number
+					case VOL_PLUS : // add mode number
 						shmaddr[0] = mode;
 						shmaddr[1] = mode_key;
 						shmaddr[2] = '\0';
+						mode = mode + 1;
+						if(mode > 4) mode = 1;
+						printf("input - mode = %d\t mode_key = %d\n",shmaddr[0], shmaddr[1]);
 						break;
-					case VOL- : // subtract mode number
+					case VOL_MINUS : // subtract mode number
 						shmaddr[0] = mode;
 						shmaddr[1] = mode_key;
 						shmaddr[2] = '\0';
-						break;
+						mode = mode - 1;
+						if(mode < 1) mode = 4;
+						printf("input - mode = %d\t mode_key = %d\n",shmaddr[0], shmaddr[1]);
 						break;
 					default :	// other key
 						shmaddr[0] = '\0';	// clear shm
-						printf("Wrong key : %d\nkey no. 116 is PROG", mode_key);	// PROG = 116번
+						printf("input(other key) - %d\t%d\n",shmaddr[0], shmaddr[1]);
 						break;
 				}
 			semunlock(sem_main);
-			sleep(3);
+			//sleep(3);
 		}
 	}// end of fork-if
 	else{		// parent - MAIN PROCESS
@@ -89,6 +100,7 @@ int main(){
 // MAIN PROCESS - parent
 		if(pid){
 			while(1){
+				shmaddr = (char *)shmat(shmid, NULL, 0);	// 0 = read/write
 				semlock(sem_main);
 					/*
 					strcpy(buf, shmaddr);	// read from shm
@@ -100,46 +112,53 @@ int main(){
 					//strcpy(buf, shmaddr);
 					switch(shmaddr[1]){	// use mode_key
 						case BACK : // exit program
+							printf("main - mode = %d\t mode_key = %d\n",shmaddr[0], shmaddr[1]);
 							shmdt(shmaddr);	// detach shm
-							exit(0);
+							//exit(0);
+							return 0;
 							break;
-						case VOL+ : // add mode number
+						case VOL_PLUS : // add mode number
 							mode = shmaddr[0];
 							mode = mode + 1;
 							if(mode>4) mode = 1;
 							shmaddr[0] = mode;
-							shmaddr[1] = '\0';
+							//shmaddr[1] = '\0';
+							printf("main - mode = %d\t mode_key = %d\n",shmaddr[0], shmaddr[1]);
 							break;
-						case VOL- : // subtract mode number
+						case VOL_MINUS : // subtract mode number
 							mode = shmaddr[0];
 							mode = mode - 1;
 							if(mode<1) mode = 4;
 							shmaddr[0] = mode;
-							shmaddr[1] = '\0';
+							//shmaddr[1] = '\0';
+							printf("main - mode = %d\t mode_key = %d\n",shmaddr[0], shmaddr[1]);
 							break;
 						default :	// other key - do nothing
+							printf("main(other key) - %d\t%d\n",shmaddr[0], shmaddr[1]);
 							break;
 					}//end of switch
 				semunlock(sem_output);
-				sleep(3);
+				//sleep(3);
 			}
-			return 0;
+			//return 0;
 		}
 // OUTPUT PROCESS - child 2
 		else{	
 			while(1){
-				//shmaddr = (char *)shmat(shmid, NULL, 0);	// 0 = read/write
+				shmaddr = (char *)shmat(shmid, NULL, 0);	// 0 = read/write
 				semlock(sem_output);
 					/*
 					printf("OUTPUT PROCESS : %s\n\n\n", shmaddr);
 					*/
 					switch(shmaddr[1]){
 						case BACK : // exit program
+							printf("output - mode = %d\t mode_key = %d\n",shmaddr[0], shmaddr[1]);
 							shmdt(shmaddr);	// detach shm
-							exit(0);
+							//exit(0);
+							//return 0;
 							break;
-						case VOL+ : // change mode + OR
-						case VOL- : // change mode -
+						case VOL_PLUS : // change mode + OR
+						case VOL_MINUS : // change mode -
 							switch(shmaddr[0]){
 								case 1 : // clock mode
 									printf("change mode to 1 : clock\n");
@@ -157,12 +176,14 @@ int main(){
 									printf("Error on calculating mode number\n");
 									break;
 							}
+							printf("output - mode = %d\t mode_key = %d\n\n",shmaddr[0], shmaddr[1]);
 							break;
 						default : 	// other key - do nothing
+							printf("output(other key) - %d\t%d\n\n",shmaddr[0], shmaddr[1]);
 							break;
 					}//end of switch
 				semunlock(sem_input);
-				sleep(3);
+				//sleep(3);
 			}
 		}
 	}// end of fork-else
--- hw1/origin_readkey.c
+++ hw1/origin_readkey.c
@@ -10,18 +10,16 @@
 #include <termios.h>
 #include <signal.h>
 #include <stdio.h>
-#include "readkey.h"
 
 #define BUFF_SIZE 64
 
 #define KEY_RELEASE 0
 #define KEY_PRESS 1
 
-int readkey (void)
+int main (int argc, char *argv[])
 {
 	struct input_event ev[BUFF_SIZE];
 	int fd, rd, value, size = sizeof (struct input_event);
-	int code=0;
 	char name[256] = "Unknown";
 
 	char* device = "/dev/input/event0";
@@ -30,26 +28,28 @@ int readkey (void)
 	}
 	// ioctl (fd, EVIOCGNAME (sizeof (name)), name);
 	// printf ("Reading From : %s (%s)n", device, name);
-	//printf("----------code = %d\n",code);
-while(!code){
-	if( (rd = read (fd, ev, size * BUFF_SIZE)) >= size){ //for read nonblocking
-		value = ev[0].value;
 
-		if (value != ' ' && ev[1].value == 1 && ev[1].type == 1){ // Only read the key press event
-			printf ("code%d\n", (ev[1].code));
+	while (1){
+		//		if ((rd = read (fd, ev, size * BUFF_SIZE)) < size)
+		//		{
+		//			printf("read()");  
+		//			return (0);     
+		//		}
+		if( (rd = read (fd, ev, size * BUFF_SIZE)) >= size){ //for read nonblocking
+			value = ev[0].value;
+
+			if (value != ' ' && ev[1].value == 1 && ev[1].type == 1){ // Only read the key press event
+				printf ("code%d\n", (ev[1].code));
+			}
+			if( value == KEY_RELEASE ) {
+				printf ("key release\n");
+			} else 				if( value == KEY_PRESS ) {
+				printf ("key press\n");
+			}
+			printf ("Type[%d] Value[%d] Code[%d]\n", ev[0].type, ev[0].value, (ev[0].code));
 		}
-		/*
-		if( value == KEY_RELEASE ) {
-			printf ("key release\n");
-		} else 				if( value == KEY_PRESS ) {
-			printf ("key press\n");
-		}
-		*/
-		//printf ("Type[%d] Value[%d] Code[%d]\n", ev[0].type, ev[0].value, (ev[0].code));
-		code = ev[0].code;
-		//printf("code = %d\n", code);
+		//printf("argc = %d, argv = %s\n", argc, *argv);
 	}
-}// while(!code)
 	close(fd);
-	return code;
+	return 0;
 }
